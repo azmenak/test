f6703357bfbfc52e1a86db5f9479ff0e6049f59f
Adam Zmenak <adam.zmenak@gmail.com>
2017-07-06 22:02:13 -0400

    Display data

--- components/Main/index.js
+++ components/Main/index.js
@@ -1,7 +1,27 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
 
-export default function Main() {
-  return (
-    <h1>Boilerplate</h1>
-  );
-}
+const Main = ({ connections, lastConnection, ownConnectionKey }) => (
+  <div>
+    <h1>Connected IPs <small>({connections.size})</small></h1>
+    <ul>
+      {connections.entrySeq().map(([key, ip]) =>
+        <li key={key}>{ip} { key === ownConnectionKey && <strong>This Connection</strong>}</li>
+      )}
+    </ul>
+
+    <h2>Last Connected User</h2>
+    <p>{lastConnection}</p>
+  </div>
+);
+
+Main.propTypes = {
+  connections: PropTypes.object,
+  lastConnection: PropTypes.string,
+  ownConnectionKey: PropTypes.string,
+};
+
+const mapStateToProps = state => state.get('data').toObject();
+
+export default connect(mapStateToProps)(Main);
